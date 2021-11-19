8dbff1e0074fdb0d4bb70ac546aa14664af08249
MICHAEL <MJENNINGS6@gmail.com>
2021-11-19 13:18:21 -0500

    wip: added models to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 # add Feeding to the import
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy, Photo
 
 # Register your models here.
 
 admin.site.register(Cat)
 # register the new Feeding Model
 admin.site.register(Feeding)
+admin.site.register(Toy)
+admin.site.register(Photo)
