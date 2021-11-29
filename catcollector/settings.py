5ccb27d625d53dbb2929b423eb22dd2303fdcaba
MICHAEL <MJENNINGS6@gmail.com>
2021-11-29 14:49:12 -0500

    cleanup: moved code around

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,5 +132,4 @@ LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
 django_heroku.settings(locals())
